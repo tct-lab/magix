fd01a116bd48648c8d3947b992d77299559d3a3b
xinglie.lkf <xinglie.lkf@taobao.com>
2018-10-23 10:13:36 +0800

    调整异步更新

--- tool/gulpfile.js
+++ tool/gulpfile.js
@@ -10,7 +10,7 @@ let ts = require('typescript');
 let customize = require('./customize');
 
 let type = 'cmd,module'; //打包kissy则type='kissy'
-let enableModules = 'style,viewInit,service,router,resource,viewMerge,viewProtoMixins,defaultView,autoEndUpdate,linkage,updateTitleRouter,urlRewriteRouter,updaterQuick';
+let enableModules = 'style,viewInit,resource,viewMerge,autoEndUpdate,linkage,updaterQuick,updaterAsync';
 
 //let mini='style,naked,updater,updaterVDOM,updaterQuick,mini'
 //let enableModules = 'style,viewInit,router,viewMerge,tipRouter,updater,autoEndUpdate,linkage,state,updaterDOM,viewProtoMixins';
